1f4c0e2fc988cc407533efac0412c31de82a1c0a
ranjana980 <76725556+ranjana980@users.noreply.github.com>
2024-03-01 18:25:55 +0000

    refactor

---
+++ src/component/home.js
@@ -0,0 +1,135 @@
+const Home = () => {
+    return (
+        <div>
+            <div class="image text-center">
+                <h1 class="font-weight-bold text-center white align-self-end">We provide two wheeler service</h1>
+                <h1 class="font-weight-bold text-center text-primary">at</h1>
+                <h1 class="font-weight-bold text-center text-primary">your doorstep</h1>
+                <h3 class="text-white">
+                    <span>
+                        <span>only at Rs.149</span>
+                        <span class="typed-cursor">|</span>
+                    </span>
+                </h3>
+            </div>
+            <div class="bg-light">
+                <h1 class="font-weight-bold text-center padding">OUR SERVICES</h1>
+                <div class="container  padding">
+                    <div class="row">
+                        <div class="col-xl-4">
+                            <div class="card-content ">
+                                <div class="div">
+                                    <img class="subsize" src="manu.png" />
+                                </div>
+                                <h5 class="text-center">Engine Repair</h5>
+                                <p>We fix engine of your vehicle at your doorsteps.</p>
+                            </div>
+                        </div>
+                        <div class="col-xl-4 ">
+                            <div class="div">
+                                <img class="subsize" src="engin.png" />
+                            </div>
+                            <h5 class="text-center">Tyres Replacement</h5>
+                            <p>We replace tyres of your vehicle at your doorsteps.</p>
+                        </div>
+                        <div class="col-xl-4">
+                            <div class="div">
+                                <img class="subsize" src="engin1.png" />
+                            </div>
+                            <h5 class="text-center">Transmission</h5>
+                            <p>We fix the transmission of your vehicle at your doorsteps.</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xl-4">
+                            <div class="div">
+                                <img class="subsize" src="engin2.png" />
+                            </div>
+                            <h5 class="text-center">Verified Mechanic</h5>
+                            <p class="text-center">Trusted and verified mechanic.</p>
+                        </div>
+                        <div class="col-xl-4">
+                            <div class="div">
+                                <img class="subsize" src="download (1).png" />
+                            </div>
+                            <h5 class="text-center">Batteries</h5>
+                            <p class="text-center">We replace battery of your vehicle at your doorsteps.</p>
+                        </div>
+                        <div class="col-xl-4">
+                            <div class="div">
+                                <img class="subsize" src="engin3.png" />
+                            </div>
+                            <h5 class="text-center">Brakes</h5>
+                            <p class="text-center">We replace brakes shoe and pads at your doorstep</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <h1 class="font-weight-bold text-center" style="margin-top: 4%;">MECHANIFY BENEFITS</h1>
+            <div class="container padding">
+                <div class="row">
+                    <div class="col-xl-4 d-flex">
+                        <img src="engin3.png" height="80" width="80" />
+                        <p class="align-self-center">100% genuine parts</p>
+                    </div>
+                    <div class="col-xl-4 d-flex">
+                        <img src="services.png" height="80" width="80" />
+                        <p class="align-self-center">Network warranty</p>
+                    </div>
+                    <div class="col-xl-4 d-flex">
+                        <img src="tyre.png" height="80" width="80" />
+                        <p class="align-self-center">Free pick and drop</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row " style="margin-top: 5%; background-color: #000;">
+                <div class="col-sm-3 dbox " style="margin-top: 2%;">
+                    <img src="logo.61c43fd0.png" height="70" width="200" style="margin-left: 25%;" />
+                </div>
+                <div class="col-sm-3 dbox " style="margin-top: 2%;">
+                    <h4 class="grey p">Address </h4>
+                    <div class="white font-weight-bold">
+                        <li type="none" class="p">Head Office :<br />
+                            Rohini Delhi - 110085</li>
+                        <li class="p" type="none">Phone: +91 - 966-793-7517</li>
+                        <li class="p" type="none">Phone: +91 - 966-795-9059</li>
+                        <li class="p" type="none">Email: Reach@mechanify.in</li>
+                    </div>
+                </div>
+                <div class="col-sm-3 dbox" style="margin-top: 2%;">
+                    <h4 class="grey p">Service</h4>
+                    <div class="white font-weight-bold">
+                        <li type="none">Engine Repair</li>
+                        <li type="none">Tyre Replacement</li>
+                        <li type="none">Transmission</li>
+                        <li type="none">Extended Warranty</li>
+                        <li type="none">Batteries</li>
+                        <li type="none">Break Repair</li>
+                    </div>
+                </div>
+                <div class="col-sm-3 dbox" style="margin-top: 2%;">
+                    <h4 class="grey p">Popular Brand</h4>
+                    <div class="white font-weight-bold">
+                        <li type="none">Royal Enfield</li>
+                        <li type="none">Suzuki</li>
+                        <li type="none">Hero</li>
+                        <li type="none">Honda</li>
+                        <li type="none">Bajaj</li>
+                        <li type="none">Yamaha</li>
+                        <li type="none">TVS</li>
+                        <li type="none">jawa</li>
+                    </div>
+                </div>
+            </div>
+            <div class="row d-flex   p" style="background-color:rgb(22, 20, 20);">
+                <div class="col-sm-6  text-center">
+                    <p style="font-size:15px; color: white;">© 2020 Mechanify Services Pvt Ltd</p>
+                </div>
+                <div class="col-sm-6 ">
+                    <img src="download.png" height="30" width="30" style="border-radius: 5px; margin-left: 70%;" />
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Home
---
+++ src/component/navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return (
+        <nav class="navbar navbar-expand-lg navbar-light bg-light d-flex flex-column ">
+            <div class="container">
+                <img class="align-self-center" src="logo.61c43fd0.png" height="70" width="200" />
+                <div class="align-self-end cotainer font-weight-bold">
+                    <a class="btn btn-light" href="main.html" role="button">Home</a>
+                    <a href="about.html" class="btn btn-light" role="button">About Us</a>
+                    <a class="btn btn-light" href="contact.html" role="button">Contact Us</a>
+                </div>
+            </div>
+
+        </nav>
+    )
+}
+export default Navbar;
